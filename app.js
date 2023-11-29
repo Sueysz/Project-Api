5eb1e0997a23f26fc9898d3f3381037efeee6119
sueys <sueys@live.fr>
2023-11-29 22:20:43 +0100

    add : env

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from "express";
 import passport from "passport"
 import cors from "cors"
+import dotenv from "dotenv"
 
 const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(cors())
 
 app.get("/",(req,res)=>{
     console.log("hello")
-})
-const port = 3000;
-app.listen(port,()=>{
-    console.log(`app listening at http://localhost:${port}`);
-})
+});
+app.listen(process.env.Port,()=>{
+    console.log(`app listening at http://localhost:${process.env.Port}`);
+});
