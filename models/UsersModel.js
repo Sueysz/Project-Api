9696faffb49bcfd49c0f56cfa3d5019a2c2a596e
sueys <sueys@live.fr>
2023-11-30 01:48:40 +0100

    create : models for user, train, station

---
+++ models/UsersModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    pseudo: {type: String, required: true},
+    password: {type: String, required: true},
+    role: {type: String, required:true, enum:["Admin","Employee","User"]}
+});
+
+export const UserModel = mongoose.model("User",UserSchema);
