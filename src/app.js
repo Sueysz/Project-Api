23dccbde5c5c4ab4e770a016879e57ccc61cc807
Sueysz <sueys@live.fr>
2023-11-30 18:25:13 +0100

    resolve conflict

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
-import passport from "passport"
-import cors from "cors"
-import dotenv from "dotenv"
+import passport from "passport";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
