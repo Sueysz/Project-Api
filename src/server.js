114b63b97cdd6243147c42ac4f891ef0645d0634
sueys <sueys@live.fr>
2023-11-30 01:49:36 +0100

    change Port to PORT for lisibility

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import "./db.js";
 
-app.listen(process.env.Port,()=>{
-    console.log(`app listening at http://localhost:${process.env.Port}`);
+app.listen(process.env.PORT,()=>{
+    console.log(`app listening at http://localhost:${process.env.PORT}`);
 });
