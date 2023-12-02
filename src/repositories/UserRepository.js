de574bbc75d388d4172926dc46a014b2a6315b72
KWS0526 <victor.yang@supinfo.com>
2023-12-02 19:35:16 +0100

    add userRepository

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,7 +1,19 @@
 import { UserModel } from "../models/UserModel.js";
 
 class UserRepository {
-
+    async listUser() {
+        const users = await UserModel.find(
+          {},
+          {
+            email: true,
+            pseudo: true,
+            password: true,
+            role: true,
+          }
+        );
+        return users;
+      }
+    
 }
 
 export default new UserRepository();
---
+++ src/routers/TrainRouter.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/train/create", (req, res) => {
+});
+
+router.get("/train", (req, res) => {
+  
+});
+
+router.put("/train/:id", (req, res) => {
+});
+
+router.delete("/train/delete/:id", (req, res) => {
+  
+});
+
+export default router;
