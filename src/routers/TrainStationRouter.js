32d03ea19a289fb100df7e0a383f9338c111c76f
KWS0526 <victor.yang@supinfo.com>
2023-12-02 19:34:00 +0100

    correction nom fichier

---
+++ src/routers/TrainStationRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+
+const router = express.Router();
+
+router.post("/train-station/create", (req, res) => {
+});
+
+router.get("/train-station", (req, res) => {
+  
+});
+
+router.put("/train-station/:id", (req, res) => {
+});
+
+router.delete("/train-station/delete/:id", (req, res) => {
+  
+});
+
+export default router;
