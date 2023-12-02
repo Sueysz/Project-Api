9aa34460a13eb8b273d2b71cd593bcbc1c6f364e
KWS0526 <victor.yang@supinfo.com>
2023-12-02 19:31:07 +0100

    correction mot book > booking

--- src/models/BookingModel.js
+++ src/models/BookingModel.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose"
 
-const BookSchema = new mongoose.Schema({
+const BookingSchema = new mongoose.Schema({
     name: {type: String, required: true},
     open_at: {type: String, required: true},
     close_at: {type: String, required: true},
     img: {type: Blob, required: true}
 });
 
-export const UserModel = mongoose.model("Book",BookSchema);
+export const BookingModel = mongoose.model("Booking",BookingSchema);
