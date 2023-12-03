e971ef8adaf84a74d19f4546f5c4fa8d4ec3e457
Sueysz <sueys@live.fr>
2023-12-03 01:04:20 +0100

    remove import

--- src/app.js
+++ src/app.js
@@ -3,16 +3,15 @@ import passport from "passport"
 import cors from "cors"
 import dotenv from "dotenv"
 // Import Router
-import BookingRouter from "./repositories/BookingRouter.js";
 import TrainRouter from "../src/routers/TrainRouter.js"
 import TrainStationRouter from "../src/routers/TrainStationRouter.js"
 import UserRouter from "./routers/UserRouter.js"
-
 // Import des models
 import { BookingModel } from "./models/BookingModel.js";
 import { TrainModel } from "./models/TrainModel.js";
 import { TrainStationModel } from "./models/TrainStationModel.js";
 import { UserModel } from "./models/UserModel.js";
+
 
 const app = express();
 dotenv.config();
