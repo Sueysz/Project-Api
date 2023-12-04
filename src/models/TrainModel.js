7c3f877ebac4f6f60da613fdef92502d19039063
Sueysz <sueys@live.fr>
2023-12-05 00:33:15 +0100

    add ref train > station

--- src/models/TrainModel.js
+++ src/models/TrainModel.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose"
 
 const TrainSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    start_station: {type: String, required: true},
-    end_station: {type: String, required: true},
+    start_station: { type: mongoose.Schema.Types.ObjectId, ref: 'TrainStation', required: true },
+    end_station: { type: mongoose.Schema.Types.ObjectId, ref: 'TrainStation', required: true },
     time_of_departure: {type: Date, required: true}
 });
 
