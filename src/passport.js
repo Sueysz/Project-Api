72cd207e24e4486f41f3a7cff65de0a8ce39007a
Sueysz <sueys@live.fr>
2023-12-04 15:03:57 +0100

    put an alias

--- src/passport.js
+++ src/passport.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { UserModel } from "./models/UserModel.js";
-import LocalStrategy from "passport-local";
+import {Strategy as LocalStrategy} from "passport-local";
 
 passport.use(new LocalStrategy(UserModel.authenticate()));
 passport.serializeUser(UserModel.serializeUser());
