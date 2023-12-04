3859ac70a8bb2388a359c3dba4c87dd26d3f6249
Sueysz <sueys@live.fr>
2023-12-05 00:32:47 +0100

    add shema zod payload

--- src/routers/zodTrainSchema.js
+++
@@ -1,8 +0,0 @@
-import {z} from "zod";
-
-export const trainCreationPayload = z.object({
-    name: z.string(),
-    start_station: z.string(),
-    end_station: z.string(),
-    time_of_departure: z.date(),
-});
