ee1af3722ff9e6f74213720e4c60b0dd6a80550e
Sueysz <sueys@live.fr>
2023-12-04 20:29:59 +0100

    add ;

--- src/routers/TrainRouter.js
+++ src/routers/TrainRouter.js
@@ -30,17 +30,17 @@ router.put("/:id", async (req, res) => {
         const train = await TrainRepository.updateTrain(id, req.body);
         res.status(200).json(train);
     } catch (error) {
-        errorHandling(res, error, "An error occurred while updating the train",)
+        errorHandling(res, error, "An error occurred while updating the train",);
     }
 
 });
 
 router.delete("/:id", async (req, res) => {
     try {
-        await TrainRepository.deleteTrain(req.params.id)
-        res.status(204).end()
+        await TrainRepository.deleteTrain(req.params.id);
+        res.status(204).end();
     } catch (error) {
-        errorHandling(res,error,"An error occurred while deleting the train")
+        errorHandling(res,error,"An error occurred while deleting the train");
     }
 });
 
