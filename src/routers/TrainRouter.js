fafa18f89cd4bc3b551808befb229a1f2c161d3e
Sueysz <sueys@live.fr>
2023-12-04 20:03:23 +0100

    add try & catch with status code

--- src/routers/TrainRouter.js
+++ src/routers/TrainRouter.js
@@ -1,27 +1,47 @@
 import express from "express";
 import TrainRepository from "../repositories/TrainRepository.js";
+import { errorHandling } from "../errorHandling.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    const train = await TrainRepository.createTrain(req.body);
-    res.status(201).json(train);
+    try {
+        const train = await TrainRepository.createTrain(req.body);
+        res.status(201).json(train);
+    } catch (error) {
+        errorHandling(res, error, "An error occurred while creating the train");
+    }
+
 });
 
 router.get("/", async (req, res) => {
-    const trains = await TrainRepository.listTrain();
-    console.log("Trains:", trains);
-    res.json(trains);
+    try {
+        const trains = await TrainRepository.listTrain();
+        res.status(200).json(trains);
+    } catch (error) {
+        errorHandling(res, error, "An error occurred while fetching the list of trains");
+    }
+
 });
 
 router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const train = await TrainRepository.updateTrain(id, req.body);
-    res.json(train);
+    try {
+        const { id } = req.params;
+        const train = await TrainRepository.updateTrain(id, req.body);
+        res.status(200).json(train);
+    } catch (error) {
+        errorHandling(res, error, "An error occurred while updating the train",)
+    }
+
 });
 
 router.delete("/:id", async (req, res) => {
-    await TrainRepository.deleteTrain(req.params.id)
+    try {
+        await TrainRepository.deleteTrain(req.params.id)
+        res.status(204).end()
+    } catch (error) {
+        errorHandling(res,error,"An error occurred while deleting the train")
+    }
 });
 
 export default router;
