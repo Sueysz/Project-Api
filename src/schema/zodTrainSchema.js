69a308118039265eb82f6a6792a3301050e8bd86
Sueysz <sueys@live.fr>
2023-12-05 20:50:06 +0100

    fix ref train

--- src/schema/zodTrainSchema.js
+++ src/schema/zodTrainSchema.js
@@ -4,7 +4,7 @@ export const trainPayload = z.object({
     name: z.string().min(1).max(30),
     start_station: z.string().min(1).max(50),
     end_station: z.string().min(1).max(50),
-    time_of_departure: z.date().refine(date => date > new Date(), { message: "Departure time must be in the future" })
+    time_of_departure: z.string().datetime().refine(date => new Date(date) > new Date(), { message: "Departure time must be in the future" })
 });
 
 export const trainStationPayload = z.object({
