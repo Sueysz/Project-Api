eab56db9d8ca47dc540bf56ae094045e143a5030
Sueysz <sueys@live.fr>
2023-12-13 21:13:26 +0100

    refacto

--- src/adminMiddleware/adminMiddleware.js
+++ src/adminMiddleware/adminMiddleware.js
@@ -2,22 +2,31 @@ import jwt from "jsonwebtoken";
 import { createTokenAdmin } from "../createTokenAdmin.js"; 
 
 export function adminMiddleware(req, res, next) {
-  const authorization = req.headers["authorization"];
-  if (!authorization) {
-    return res.status(403).json("Forbidden");
+  const authorizationHeader = req.headers["authorization"];
+
+  if (!authorizationHeader) {
+    return res.status(403).json("Forbidden: Authorization header missing");
   }
 
-  const token = authorization.replace("Bearer ", "");
-  const secretKey = process.env.TOKEN_SECRET; 
+  const token = authorizationHeader.replace("Bearer ", "");
+  const secretKey = process.env.TOKEN_SECRET;
+
+  if (!secretKey) {
+    return res.status(500).json("Internal Server Error: Token secret is missing");
+  }
 
   jwt.verify(token, secretKey, (err, user) => {
-    if (err || user.role !== "Admin") {
-      return res.status(403).json("Forbidden");
+    if (err || !isAdmin(user)) {
+      return res.status(403).json("Forbidden: Invalid or insufficient privileges");
     }
 
     req.user = user;
     next();
   });
+}
+
+function isAdmin(user) {
+  return user && user.role === "Admin";
 }
 
 const generatedToken = createTokenAdmin();
