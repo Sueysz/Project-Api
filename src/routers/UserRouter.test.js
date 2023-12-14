cc4835f739011c36c37deec2d11553ceeea99717
Sueysz <sueys@live.fr>
2023-12-14 10:01:34 +0100

    add test for delete post and put

--- src/routers/UserRouter.test.js
+++ src/routers/UserRouter.test.js
@@ -29,6 +29,59 @@ describe("GET /users", () => {
         expect(response.status).toEqual(404);
 
     });
+
+    describe("POST /users", () => {
+        it("[201] creates a new user", async () => {
+            const newUser = {
+                username: "newuser",
+                email: "newuser@example.com",
+                password: "newpassword",
+                role: "User",
+            };
+
+            const response = await request.post("/users").send(newUser);
+
+            expect(response.status).toEqual(201);
+        });
+    });
+
+    describe("PUT /users/:id", () => {
+        it("[200] updates a user by ID", async () => {
+            const userToUpdate = await UserModel.create({
+                username: "updateuser",
+                email: "updateuser@example.com",
+                password: "updatepassword",
+                role: "User",
+            });
+
+            const updatedUserData = {
+                username: "updateduser",
+                email: "updateduser@example.com",
+                role: "Admin",
+            };
+
+            const adminToken = createTokenAdmin();
+            const response = await request.put(`/users/${userToUpdate._id}`).set("authorization", `Bearer ${adminToken}`).send(updatedUserData);
+
+            expect(response.status).toEqual(200);
+        });
+
+    });
+
+    describe("DELETE /users/delete/:id", () => {
+        it("[204] deletes a user by ID", async () => {
+            const userToDelete = await UserModel.create({
+                username: "deleteuser",
+                email: "deleteuser@example.com",
+                password: "deletepassword",
+                role: "User",
+            });
+
+            const adminToken = createTokenAdmin();
+            const response = await request.delete(`/users/delete/${userToDelete._id}`).set("authorization", `Bearer ${adminToken}`).send();
+
+            expect(response.status).toEqual(204);
+        });
+
+    });
 });
-
-
