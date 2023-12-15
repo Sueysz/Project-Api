19cf8a942ad73a4299db4da82e27a4949e15cadc
Sueysz <sueys@live.fr>
2023-12-15 04:32:04 +0100

    fix zod shema

--- src/schema/zodSchema.js
+++ src/schema/zodSchema.js
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
-export const trainPayload = z.object({
+export const TrainPayload = z.object({
     name: z.string().min(1).max(30),
     start_station: z.string().min(1).max(50),
     end_station: z.string().min(1).max(50),
     time_of_departure: z.string().datetime().refine(date => new Date(date) > new Date(), { message: "Departure time must be in the future" })
 });
 
-export const trainStationPayload = z.object({
+export const TrainStationPayload = z.object({
     name: z.string(),
     open_at: z.string(),
     close_at: z.string(),
@@ -15,10 +15,8 @@ export const trainStationPayload = z.object({
 });
 
 export const UserCreateSchema = z.object({
-    email: z.string(),
+    email: z.string().email(),
     username: z.string(),
     password: z.string().min(4),
-    role: z.string(),
+    role: z.string().optional(),
   });
-
-
