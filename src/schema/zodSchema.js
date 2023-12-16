f6dcfb21e68bad4190f49eaaa2d71373424a7d42
Sueysz <sueys@live.fr>
2023-12-17 00:48:34 +0100

    add strict mode & remove role from schema

--- src/schema/zodSchema.js
+++ src/schema/zodSchema.js
@@ -5,18 +5,17 @@ export const TrainPayload = z.object({
     start_station: z.string().min(1).max(50),
     end_station: z.string().min(1).max(50),
     time_of_departure: z.string().datetime().refine(date => new Date(date) > new Date(), { message: "Departure time must be in the future" })
-});
+}).strict();
 
 export const TrainStationPayload = z.object({
     name: z.string(),
     open_at: z.string(),
     close_at: z.string(),
     img: z.string(),
-});
+}).strict();
 
 export const UserCreateSchema = z.object({
     email: z.string().email(),
     username: z.string(),
     password: z.string().min(4),
-    role: z.string().optional(),
-  });
+  }).strict();
