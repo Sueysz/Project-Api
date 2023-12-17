ad70aa7a30b2354b4e699ac48345cea08dae75f8
Sueysz <sueys@live.fr>
2023-12-17 15:00:41 +0100

    delete session we not use it

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
-import { sessionMiddleWare } from "./session.js";
 
 // Import Router
 import BookingRouter from "./routers/BookingRouter.js";
@@ -9,14 +8,14 @@ import TrainRouter from "./routers/TrainRouter.js"
 import TrainStationRouter from "./routers/StationRouter.js"
 import UserRouter from "./routers/UserRouter.js"
 import { errorHandling } from "./utils/errorHandling.js";
+import { autoCatch } from "./utils/handler.js";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(sessionMiddleWare);
 app.use(cors())
 
-
+app.use(autoCatch)
 app.use("/booking", BookingRouter);
 app.use("/trains", TrainRouter);
 app.use("/trains-stations", TrainStationRouter);
