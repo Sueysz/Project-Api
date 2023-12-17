bce7aa56e4f18cdcffccdefcda9bcb8c0dc68828
Sueysz <sueys@live.fr>
2023-12-17 20:49:48 +0100

    add swagger

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
 
+import swaggerUi from "swagger-ui-express";
+import { parse } from "yaml";
+import fs from "node:fs/promises";
+
 // Import Router
 import BookingRouter from "./routers/BookingRouter.js";
 import TrainRouter from "./routers/TrainRouter.js"
 import TrainStationRouter from "./routers/StationRouter.js"
 import UserRouter from "./routers/UserRouter.js"
 import { errorHandling } from "./utils/errorHandling.js";
+
+const swaggerDocument = parse(await fs.readFile("./openapi.yaml", "utf8"));
 
 const app = express();
 dotenv.config();
@@ -18,6 +24,7 @@ app.use("/booking", BookingRouter);
 app.use("/trains", TrainRouter);
 app.use("/trains-stations", TrainStationRouter);
 app.use("/users", UserRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use((error,req,res,next)=>{
     errorHandling(res,{error})
 });
