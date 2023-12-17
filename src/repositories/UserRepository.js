120d41123dbbae5f39781f08416a7e2c0d6afe55
Sueysz <sueys@live.fr>
2023-12-17 03:51:45 +0100

    get imagebyid

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,43 +1,72 @@
 import { UserModel } from "../models/UserModel.js";
+import { errorHandling } from "../utils/errorHandling.js";
 
 class UserRepository {
-    async listUser() {
-        const users = await UserModel.find(
-          {},
-          {
-            email: true,
-            pseudo: true,
-            password: true,
-            role: true,
-          }
-        );
-        return users;
+  async listUser() {
+    const users = await UserModel.find(
+      {},
+      {
+        email: true,
+        pseudo: true,
+        password: true,
+        role: true,
       }
-    async getById(id) {
-      return await UserModel.findById(id);
+    );
+    return users;
+  }
+
+  async getByEmail(email) {
+    try {
+      return await UserModel.findOne({ email: email});
+    } catch (error) {
+      if (error.message.startsWith('Cast to ObjectId failed for value "')) {
+        return null
+      }
+      throw error
     }
-    async deleteUser(id) {
-      const { deletedCount } = await UserModel.deleteOne({_id: id})
-      if (deletedCount !== 1) {
-        throw new Error("user not found");
+  }
+
+  async getById(id) {
+    try {
+      return await UserModel.findOne({ _id: id });
+    } catch (error) {
+      if (error.message.startsWith('Cast to ObjectId failed for value "')) {
+        return null
       }
+      throw error
     }
 
-    async createUser(payload) {
-      const user = await UserModel.create(payload);
-  
-      return user;
+  }
+  async deleteUser(id) {
+    const { deletedCount } = await UserModel.deleteOne({ _id: id })
+    if (deletedCount !== 1) {
+      throw new Error("user not found");
     }
+  }
 
-    async updateUser(id, payload) {
-      const upUser = await UserModel.findOneAndUpdate(
+  async createUser(payload) {
+    const user = await UserModel.create(payload);
+
+    return user;
+  }
+
+  async updateUser(id, payload) {
+    try {
+      return await UserModel.findOneAndUpdate(
         {
           _id: id,
         },
         payload
       );
-      return upUser;
+    } catch (error) {
+      if (error.message.startsWith('Cast to ObjectId failed for value "')) {
+        return null
+      }
+      throw error
     }
+
+  }
+
 }
 
 export default new UserRepository();
